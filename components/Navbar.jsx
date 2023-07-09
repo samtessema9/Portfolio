9e395698edc83d2bd29d4625ff29c5bf784c10c0
sam tessema <99148321+samtessema9@users.noreply.github.com>
2023-07-08 17:11:48 -0700

    C:/Program Files/Git/homepage completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,14 +3,28 @@ import './index.css'
 import StorageIcon from '@mui/icons-material/Storage';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import CodeIcon from '@mui/icons-material/Code';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 
 
 const Navbar = () => {
     return ( 
         <div id="navbar">
-            <CodeIcon className='icon' fontSize='large'/>
-            <GitHubIcon className='icon' fontSize='large'/>
-            <StorageIcon className='icon' fontSize='large'/>
+            <a>
+                <ArticleOutlinedIcon className='icon' fontSize='large'/>
+                <p>resume</p>
+            </a>
+            <a href='https://github.com/samtessema9' target='_blank'>
+                <GitHubIcon className='icon' fontSize='large'/>
+                <p>github</p>
+            </a>
+            <a>
+                <CodeIcon className='icon' fontSize='large'/>
+                <p>projects</p>
+            </a>
+            <a>
+                <CodeIcon className='icon' fontSize='large'/>
+                <p>projects</p>
+            </a>
         </div>
      );
 }
